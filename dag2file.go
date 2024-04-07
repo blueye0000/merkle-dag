ea408b897f4c78d64fabd524d315daa24daedb8a
blueye0000 <2960594561@qq.com>
2024-04-07 15:28:16 +0800

    second

--- dag2file.go
+++ dag2file.go
@@ -3,5 +3,12 @@ package merkledag
 // Hash to file
 func Hash2File(store KVStore, hash []byte, path string, hp HashPool) []byte {
 	// 根据hash和path， 返回对应的文件, hash对应的类型是tree
-	return nil
+	// 从 KVStore 中获取数据
+	data, err := store.Get(hash)
+	if err != nil {
+		// 处理错误
+		return nil
+	}
+	// 返回对应的文件内容
+	return data
 }
